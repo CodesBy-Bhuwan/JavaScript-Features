bf55e36c03b78ed48b0e8a47224478e0411cd2df
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2025-01-29 23:44:54 +0545

    Completion

--- Drag&Drop/index.js
+++ Drag&Drop/index.js
@@ -4,17 +4,46 @@
 
 */
 
-const dragges= document.querySelector(".draggable");
-const containers = document.querySelector(".containers");
+const draggables= document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
 
 // First the item will be dragged by triggering event
-dragges.forEach(dragges => {
-    dragges.addEventListener('dragstart', ()=>{
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', ()=>{
         // console.log("drag-start")
-        dragges.classList.add('dragging');//On dragging container item will be equipped means add classname on clicked within container
+        draggable.classList.add('dragging');//On dragging container item will be equipped means add classname on clicked within container
     })
-    dragges.addEventListener("dragstop",  ()=>{
-        dragges.classList.remove('dragging')
+    draggable.addEventListener("dragend",  ()=>{
+        draggable.classList.remove('dragging')
     })
-    
 });
+
+containers.forEach(container=>{
+    container.addEventListener('dragover', e => {
+        // console.log("dragover");
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const draggable = document.querySelector('.dragging');
+        if (afterElement == null){
+            container.appendChild(draggable);}
+            else{
+                container.insertBefore(draggable, afterElement);
+            }
+    })
+})
+
+function getDragAfterElement(container, y){
+    // will drag not everyitems but only that element that has classname draggable and dragging
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child)=>{
+        const box= child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        // console.log(box);
+        if(offset < 0 && offset > closest.offset){
+            return{offset: offset, element: child}
+        }else{
+            return closest
+        }
+    },{offset: Number.NEGATIVE_INFINITY, element:null}).element;
+}
